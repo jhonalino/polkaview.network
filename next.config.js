1919aa294abca4439b00244d320d28d202cf74b3
jhonalino <jhonfrancisalino@gmail.com>
2021-05-17 09:52:23 -0700

    test

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
       {
         source: '/',
         destination: '/dot',
-        permanent: true,
+        permanent: false,
       },
     ]
   },
--- redisconn.js
+++
@@ -1,6 +0,0 @@
-const redisconn = {
-    host: process.env.REDIS_HOST || 'localhost',
-    port: process.env.REDIS_PORT || 6379
-};
-
-module.exports = redisconn;
