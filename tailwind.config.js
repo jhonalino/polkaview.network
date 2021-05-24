74cc25abef0f91af081c235b5882556ee70fca95
jhonalino <jhonfrancisalino@gmail.com>
2021-05-24 06:41:48 -0700

    updated config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
     purge: {
         content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
         options: {
-            safelist: ['text-dot', 'text-ksm']
+            safelist: ['text-dot', 'text-ksm', 'table-dot', 'table-ksm']
         }
     },
     darkMode: false, // or 'media' or 'class'
