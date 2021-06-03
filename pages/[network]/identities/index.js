2a81d81f366a0c2912f4d170cf7f8c0926e6573c
jhonalino <jhonfrancisalino@gmail.com>
2021-06-02 19:33:39 -0700

    added identities

--- pages/[network]/identities/index.js
+++ pages/[network]/identities/index.js
@@ -22,6 +22,30 @@ function getIdentityDetails(identity) {
 
         var judgements = identity.judgements;
 
+
+        var colorMap = {
+            unknown: 'text-gray-300',
+            feePaid: 'text-gray-400',
+            reasonable: 'text-green-400',
+            knownGood: 'text-yellow-500',
+            outOfDate: 'text-red-400',
+            lowQuality: 'text-red-300',
+            erroneous: 'text-red-500',
+        };
+
+        judgements = judgements.map(function ([index, result]) {
+
+            var result = Object.keys(result)[0];
+
+            return {
+                index,
+                result: result,
+                textColorClass: colorMap[result]
+            };
+
+        });
+
+
         identity = identity.info;
 
         identity = {
@@ -52,50 +76,64 @@ export default function Index() {
     const [registrar, setRegistrars] = useState([]);
 
 
-    useEffect(async function () {
+    useEffect(function () {
+        async function fetch() {
 
-        const wsProvider = new WsProvider('wss://polkadot-node.polkaview.network');
+            const wsProvider = new WsProvider('wss://polkadot-node.polkaview.network');
 
-        const api = new ApiPromise({ provider: wsProvider });
+            const api = new ApiPromise({ provider: wsProvider });
 
-        await api.isReady;
+            await api.isReady;
 
-        var registrars = await api.query.identity.registrars();
+            var registrars = await api.query.identity.registrars();
 
-        registrars = registrars.toHuman();
+            registrars = registrars.toHuman();
 
-        console.log(registrars);
+            console.log(registrars);
 
-        setRegistrars(registrars);
+            setRegistrars(registrars);
 
-        let identities = await api.query.identity.identityOf.entries();
+            let identities = await api.query.identity.identityOf.entries();
 
-        identities = await async.mapLimit(identities, async function ([key, identity]) {
+            async.map(identities, async function ([key, identity], cb) {
+                var address = key.args[0].toHuman();
+                var details = await getIdentityDetails(identity.toJSON());
 
-            var address = key.args[0].toHuman();
-            var details = await getIdentityDetails(identity.toJSON());
+                var nominator = await api.query.staking.nominators(address);
+                var isNominator = !nominator.isEmpty;
 
-            var nominator = await api.query.staking.nominators(address);
-            var isNominator = !nominator.isEmpty;
+                var validator = await api.query.staking.validators(address);
+                var isValidator = !validator.isEmpty;
 
-            var validator = await api.query.staking.validators(address);
-            var isValidator = !validator.isEmpty;
+                var result = {
+                    address,
+                    ...details,
+                    isValidator,
+                    isNominator
+                };
 
-            console.log(address);
+                cb(null, result)
+                console.log('e');
 
-            return {
-                ...address,
-                ...details,
-                isValidator,
-                isNominator
-            };
+            }, function (err, result) {
 
-        });
+                if (err) {
+                    console.log(err, 'err');
+                    return;
+                }
+
+                console.log('iden', result);
+                setIdentities(result);
+
+            });
 
 
-        console.log('iden', identities);
-        setIdentities(identities);
-
+        }
+        console.log('effect')
+        fetch();
+        return function () {
+            console.log('cleanup effect');
+        }
     }, [])
 
     console.log('testtt')
@@ -117,7 +155,7 @@ export default function Index() {
                 <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&family=Raleway&display=swap" rel="stylesheet" />
             </Head>
 
-            <div className="w-full max-w-screen-xl min-h-screen">
+            <div className="w-full max-w-screen-2xl min-h-screen">
                 <header className="flex p-4 items-center justify-between">
                     <div className="flex items-center justify-start">
                         <div>
@@ -134,10 +172,10 @@ export default function Index() {
                     </div>
                 </header>
                 <div className="flex flex-wrap items-center justify-center">
-                    {identities && identities.map(function ({ address, display, legal }) {
+                    {identities && identities.map(function ({ address, display, legal, isValidator, isNominator, judgements }) {
                         return (
                             <div key={address} className="text-white p-1 box-border">
-                                <div className='w-96 h-64 flex flex-col items-center justify-center bg-kinda-black rounded-sm'>
+                                <div className='w-72 h-64 flex flex-col items-center justify-center bg-kinda-black rounded-sm'>
                                     <div className="identicon-container mb-4 border-2 border-dot rounded-full box-content p-2">
                                         <Identicon
                                             value={address}
@@ -152,6 +190,24 @@ export default function Index() {
                                         <p className="text-gray-400 text-sm">
                                             {legal}
                                         </p>
+                                        <div className="flex flex-wrap justify-center">
+                                            {isValidator && (
+                                                <span className="text-blue-400 inline-block mx-1">
+                                                    validator
+                                                </span>)
+                                            }
+                                            {isNominator && (
+                                                <span className="text-purple-400 inline-block mx-1">
+                                                    nominator
+                                                </span>)
+                                            }
+                                            {judgements.map(function ({ index, result, textColorClass }) {
+                                                console.log(textColorClass);
+                                                return (<span key={index} className={`${textColorClass} inline-block mx-1`}>
+                                                    {result}
+                                                </span>)
+                                            })}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
