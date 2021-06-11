c9dfa0f036da7e98081d5915c51157e30a24cfc3
jhonalino <jhonfrancisalino@gmail.com>
2021-06-11 12:15:03 -0700

    added price cached by coingecko

--- pages/api/[network]/price.js
+++ pages/api/[network]/price.js
@@ -2,12 +2,32 @@ import async from 'async';
 import Promise from 'bluebird';
 import redis from 'redis';
 Promise.promisifyAll(redis.RedisClient.prototype);
+import Cors from 'cors'
+// Initializing the cors middleware
+const cors = Cors({
+    methods: ['GET', 'HEAD'],
+})
 
 const client = redis.createClient({
     host: 'redis'
 });
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result) => {
+            if (result instanceof Error) {
+                return reject(result)
+            }
+
+            return resolve(result)
+        })
+    })
+}
 
 export default async function handler(req, res) {
+
+    await runMiddleware(req, res, cors);
 
     var network = req.query.network;
     network = network ? network.toLowerCase() : "dot";
