642e506a814666bcb45414c40cf189fa5b3e64a5
Dee Evans <deekor88@gmail.com>
2021-07-11 15:18:07 -0600

    add polkaview source to dotscanner link

--- pages/[network]/staking/index.js
+++ pages/[network]/staking/index.js
@@ -62,7 +62,7 @@ const StatDisplay = function (props) {
                 {props.suffixFull === 'polkadot' &&
 
                     <div>
-                        <a className="text-blue-500 text-md lowercase" target="_blank" href={`https://dotscanner.com/${props.suffixFull}/account/${props.address}`}>
+                        <a className="text-blue-500 text-md lowercase" target="_blank" href={`https://dotscanner.com/${props.suffixFull}/account/${props.address}?utm_source=polkaview`}>
                             <span className="mr-1">DotScanner</span>
                             <svg className="inline" width="12px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
