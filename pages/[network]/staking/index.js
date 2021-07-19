a6ea22006f4d5ccad849b1e9c8e0edfdb19457b3
jhonalino <jhonfrancisalino@gmail.com>
2021-07-18 20:40:50 -0700

    added dotsanner kusama

--- pages/[network]/staking/index.js
+++ pages/[network]/staking/index.js
@@ -59,18 +59,16 @@ const StatDisplay = function (props) {
             </CountUp>
 
             <div className="">
-                {props.suffixFull === 'polkadot' &&
 
-                    <div>
-                        <a className="text-blue-500 text-md lowercase" target="_blank" href={`https://dotscanner.com/${props.suffixFull}/account/${props.address}?utm_source=polkaview`}>
-                            <span className="mr-1">DotScanner</span>
-                            <svg className="inline" width="12px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
-                            </svg>
-                        </a>
-                    </div>
+                <div>
+                    <a className="text-blue-500 text-md lowercase" target="_blank" href={`https://dotscanner.com/${props.suffixFull}/account/${props.address}?utm_source=polkaview`}>
+                        <span className="mr-1">DotScanner</span>
+                        <svg className="inline" width="12px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                        </svg>
+                    </a>
+                </div>
 
-                }
                 <div>
                     <a className="text-blue-500 text-md lowercase" target="_blank" href={`https://polkascan.io/${props.suffixFull}/account/${props.address}`}>
                         <span className="mr-1">polkascan</span>
